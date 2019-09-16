f4e2aa5d16d0ff4c87b08a1790117058cfd2ab0d
mhaa87 <47102878+mhaa87@users.noreply.github.com>
2019-09-16 10:41:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ const dbName = "cv_portal";
 const sessionDuration = 3 * 24 * 60 * 60 * 1000;
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-// var hash = bcrypt.hashSync(user.password, saltRounds);
-// bcrypt.compareSync(ctx.request.body.password, user.password)
 var profileCollection;
 var sessionCollection;
+var commentCollection;
 var databaseConnected = false;
 
 app.use(logger());
@@ -24,20 +23,25 @@ app.use(cors());
 app.use(BodyParser());
 
 router.use("/", checkConnection);
+router.use("/admin", validateAdmin);
 router.post("/saveCV", saveCV);
 router.post("/login", login);
-router.post("/cvMenu", cvMenu);
 router.post("/getCV", getCV);
 router.post("/getLastCV", getLastCV);
 router.post("/delete", deleteCV);
 router.post("/deleteAll", deleteAll);
+router.post("/addComment", addComment);
+router.post("/deleteComment", deleteComment);
+router.post("/getComments", getComments);
+router.post("/admin/getUsers", getUsers);
 router.get("/fonts", getFonts);
 
 async function checkConnection(ctx, next){
     if(databaseConnected === false){
         await client.connect().then(async function(){
-            profileCollection = await client.db(dbName).collection("profiles");
-            sessionCollection = await client.db(dbName).collection("sessions");
+            profileCollection = client.db(dbName).collection("profiles");
+            sessionCollection = client.db(dbName).collection("sessions");
+            commentCollection = client.db(dbName).collection("comments");
             databaseConnected = true;
             // cleanSessions();
             await next();
@@ -47,10 +51,33 @@ async function checkConnection(ctx, next){
     }
 }
 
+async function validateAdmin(ctx, next){
+    var profile = await GetProfileByKey(ctx.request.body.key);
+    if(profile.admin) await next();
+    else ctx.body = {status: false, msg: "Error validating user as admin"};
+}
+
+async function addComment(ctx){
+    var comment = ctx.request.body;
+    comment.date = Date.now();
+    ctx.body = await commentCollection.insertOne(comment);
+}
+
+async function deleteComment(ctx){
+    ctx.body = await commentCollection.remove({date: ctx.request.body});
+}
+
+async function getComments(ctx){
+    ctx.body = await commentCollection.find().toArray();
+}
+
+async function getUsers(ctx){
+    ctx.body = await profileCollection.find().project({'_id': 0, 'name': 1, 'email': 1, 'admin': 1}).toArray();
+}
+
 async function GetProfileByEmail(email){
     var res = await profileCollection.findOne({"email": email})
-    if(res === null) return false;
-    return res;
+    return (res === null) ? false : res;
 }
 
 async function GetProfileByKey(key){
@@ -69,25 +96,29 @@ async function createUser(user, ip){
     if(profile !== false){return {status: false, msg: "Email address is taken"}};
     user.password = bcrypt.hashSync(user.password, saltRounds);
     await profileCollection.insertOne(user);
-    return {status: true, msg: "New profile created", "name": user.name, "key": await getKey(user, ip)}
+    return {status: true, msg: "New profile created", "name": user.name, "key": await getKey(user, ip), "cvList": []}
 }
 
 async function login(ctx) {
-    console.log("logging in");
+    // console.log("logging in");
     var user = ctx.request.body.user;
     if(ctx.request.body.createUser) {ctx.body = await createUser(user, ctx.request.ip); return}
     if(ctx.request.body.key != false && user == false){ctx.body = await autoLogin(ctx.request.body.key);return}
     profile = await GetProfileByEmail(user.email); 
     if(profile == false){ctx.body = {status: false, msg: "Profile does not exist"}; return}
     else if(bcrypt.compareSync(user.password, profile.password)){ctx.body = {status: false, msg: "Incorrect password"}; return}
-    ctx.body = {"status": true, "msg": "Login successfull", "name": profile.name, "key": await getKey(user, ctx.request.ip)}
+    ctx.body = {"status": true, "msg": "logged in", 
+        "name": profile.name, "isAdmin": profile.admin,
+        "key": await getKey(user, ctx.request.ip),
+        "cvList": profile.cvList,
+    }
 }
 
 async function autoLogin(key) {
     var profile = await GetProfileByKey(key);
     if(profile == false){return {status: false, msg: "Session key " + key + " not found"}};
     await sessionCollection.updateOne({"key": key}, {$set: {"expDate": Date.now() + sessionDuration}});
-    return {"status": true, "msg": "logged in", "name": profile.name, "key": key};
+    return {"status": true, "msg": "logged in", "name": profile.name, "key": key, "isAdmin": profile.admin, "cvList": profile.cvList,};
 }
 
 async function getKey(user, ip){
@@ -105,18 +136,21 @@ async function getKey(user, ip){
 async function saveCV(ctx){
     var profile = await GetProfileByKey(ctx.request.body.key);
     var content = ctx.request.body.content;
+    cvNameCheck(content);
     await profileCollection.updateOne({"email": profile.email}, {$pull: {"cvList": {"cvName" : content.cvName}}});
     await profileCollection.updateOne({"email": profile.email}, {$addToSet:{'cvList': {"cvName": content.cvName, "content": content}}});
     await profileCollection.updateOne({"email": profile.email}, {$set:{'lastCV': content.cvName}});
-    ctx.body = true;
+    ctx.body = (await GetProfileByEmail(profile.email)).cvList;
 }
 
-async function cvMenu(ctx){
-    var profile = await GetProfileByKey(ctx.request.body.key);
-    if(profile === false){ctx.body = {status: false, msg: "Error: could not find profile"}; return};
-    ctx.body = {status: true, msg: ""}
-    if(profile.cvList == false || profile.cvList == undefined){ctx.body = []; return};
-    ctx.body.cvList = profile.cvList.map(e => e.cvName);
+async function cvNameCheck(content){
+    if(content.name.length < 1) content.name = "...";
+    if(content.cvName.length < 1) content.cvName = "...";
+    content.personInfo.forEach((item, i, list) => {if(item.title.length <1) item.title = "..."});
+    content.mainContent.forEach((item, i, list) => {
+        if(item.title.length <1) item.title = "...";
+        if(item.type === 'list') item.items.forEach((listItem, i, list) => {if(listItem.title.length <1) listItem.title = "..."});
+    });
 }
 
 async function getCV(ctx){
@@ -134,7 +168,7 @@ async function getLastCV(ctx){
 async function deleteCV(ctx){
     var profile = await GetProfileByKey(ctx.request.body.key);
     await profileCollection.updateOne({"email": profile.email}, {$pull: {"cvList": {"cvName" : ctx.request.body.cvName}}});
-    ctx.body = true;
+    ctx.body = (await GetProfileByEmail(profile.email)).cvList.map(e => e.cvName);
 }
 
 async function deleteAll(ctx){
